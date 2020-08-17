886f20b01466537b45e7c7934df5e45c714cea00
yanzhifanghuoqiang <854855734@qq.com>
2020-08-17 17:18:30 +0800

    Update yuehuiba.py

--- bilibili/yuehuiba.py
+++ bilibili/yuehuiba.py
@@ -1,27 +1,31 @@
 import requests
 import parsel
 
-url = 'https://tieba.baidu.com/f?ie=utf-8&kw=%E7%BA%A6%E4%BC%9A'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-response = requests.get(url, headers=headers)
-# print(response)
-html_data = response.text
-html = parsel.Selector(html_data)
-title_url = html.xpath('//div[@class="threadlist_lz clearfix"]/div/a/@href').getall()
-# print(title_url)
+page_num = 0
+for page in range(0, 56500 + 1, 50):
+    page_num += 1
+    print('==========正在爬取第{}页=========='.format(page_num))
+    url = 'https://tieba.baidu.com/f?kw=%E7%BA%A6%E4%BC%9A&ie=utf-8&pn={}'.format(str(page))
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'}
+    response = requests.get(url, headers=headers)
+    # print(response)
+    html_data = response.text
+    html = parsel.Selector(html_data)
+    title_url = html.xpath('//div[@class="threadlist_lz clearfix"]/div/a/@href').getall()
+    # print(title_url)
 
-second_url = 'https://tieba.baidu.com'
-for url in title_url:
-    all_url = second_url + url
-    print(all_url)
+    second_url = 'https://tieba.baidu.com'
+    for url in title_url:
+        all_url = second_url + url
+        print(all_url)
 
-    response_2 = requests.get(url=all_url, headers=headers).text
-    response_2_data = parsel.Selector(response_2)
-    result_list = response_2_data.xpath('//cc/div/img[@class="BDE_Image"]/@src').getall()
+        response_2 = requests.get(url=all_url, headers=headers).text
+        response_2_data = parsel.Selector(response_2)
+        result_list = response_2_data.xpath('//cc/div/img[@class="BDE_Image"]/@src').getall()
 
-    for li in result_list:
-        img_data = requests.get(url=li, headers=headers).content
-        file_name = li.split('/')[-1]
-        with open('img\\' + file_name, mode='wb') as f:
-            f.write(img_data)
-            print('正在保存:', file_name)
+        for li in result_list:
+            img_data = requests.get(url=li, headers=headers).content
+            file_name = li.split('/')[-1]
+            with open('img\\' + file_name, mode='wb') as f:
+                f.write(img_data)
+                print('正在保存:', file_name)
